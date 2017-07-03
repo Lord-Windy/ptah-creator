972954a29853779a4845f7fdda56af6037a5529b
sam <sam@samuelbrown.me>
2017-07-04 09:46:30 +1000

    Adding basic states in now so I can merge with Jesse

---
+++ src/model/DefaultStates.js
@@ -0,0 +1,24 @@
+
+
+
+export function createDefaultFunction() {
+    return {
+        ID: -1,
+        Name: "",
+        Description: "",
+        Values: []
+    };
+}
+
+export function createDefaultState() {
+    return {
+        StartingID: 0,
+        ProjectName: "Place title here",
+        Description: "The description",
+        Author: "The Authors",
+        UserID: -1,
+        Characters: [],
+        Functions: [],
+        Nodes: [],
+    };
+}
